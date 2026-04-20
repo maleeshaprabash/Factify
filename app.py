0b2e9816d0e4191a2b3310144898a72775c36205
maleeshaprabash <maleeshaprabash9@gmail.com>
2026-04-20 17:02:17 +0530

    change

--- app.py
+++ app.py
@@ -94,5 +94,6 @@ def health():
 
 
 if __name__ == "__main__":
-    print("Starting Fake News Classifier API at http://localhost:5000")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Starting Fake News Classifier API at http://0.0.0.0:{port}")
+    app.run(host="0.0.0.0", port=port, debug=False)
